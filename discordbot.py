cf8490e3a0533709e3d79ceff5e10f82df8a63a0
nikhilodeon2001 <35127542+nikhilodeon2001@users.noreply.github.com>
2025-08-27 13:58:26 -0700

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12363,7 +12363,7 @@ async def start_trivia():
             #await ask_list_question("TheOkraG", 591861826690613248, 3)
             #await ask_chaos_challenge("TheOkraG",591861826690613248, 23)
             #await ask_tally_challenge("TheOkraG",591861826690613248, 3)
-            await ask_stock_challenge("TheOkraG",591861826690613248, 3)
+            #await ask_stock_challenge("TheOkraG",591861826690613248, 3)
 
             round_responders.clear()  # Reset round responders
             round_data["questions"] = []
