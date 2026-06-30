d5b5b93811585620db799b9dc2e578f7844c0e60
Nikhil Sharma <nikhilodeon2001@users.noreply.github.com>
2026-06-30 07:30:31 -0700

    Bold the O K R A letters in Wheel of Fortune prompt

--- discordbot.py
+++ discordbot.py
@@ -15867,7 +15867,7 @@ async def ask_wof_letters(winner, answer, extra_time, winner_id, clue):
     answer = answer.upper()
     start_time = time.time()
     await safe_send(channel, f"\u200b\n**<@{winner_id}>**:❓**Pick {num_wf_letters} Letters**❓\n" +
-                       (f"\n🥒 I'll give you O K R A 🥒\n\u200b" if fixed_letters else ""))    
+                       (f"\n🥒 I'll give you **O K R A** 🥒\n\u200b" if fixed_letters else ""))    
     
     wf_letters = []
 
